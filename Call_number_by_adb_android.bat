fbc0f7c1a9dcc0389882279bd8e28e1dc49e6a6a
jzomerfeld <zomer331@gmail.com>
2018-11-30 14:32:36 +0100

    v2
    Added message - "calling"
    skiping choosing number when %input% is empty

--- Call_number_by_adb_android.bat
+++ Call_number_by_adb_android.bat
@@ -1,11 +1,32 @@
+mode con:cols=60 lines=10
 @echo off
-cls
+	COLOR 7e
+	TITLE [Phone call - android, ADB must be installed]
+@echo off
 :begin
-set /p UserInputPath=Hey! What number would You like?
-adb -s "9885e6534e424f414e" shell am start -a android.intent.action.CALL -d tel:"%UserInputPath%"
-
-if %UserInputPath%==%cls% goto begin
-pause 4s
-
+cls
+:: Asking user for number to call / inserting the number variable and -call
+	set /p UserInputPath=Hey! What number would You like to call?
+REM		if %UserInputPath% equ=="%cls%" goto begin
+	IF [%UserInputPath%] equ [] goto begin
+:main
+@echo off
+	adb -s "9885e6534e424f414e" shell am start -a android.intent.action.CALL -d tel:"%UserInputPath%"
+:msg call
+		color fa
+		@echo [-------------------------------------]
+		@echo [-------------------------------------]
+		@echo [---------------Calling---------------]
+		@echo [--------------%UserInputPath%--------------]
+		@echo [-------------------------------------]
+				
+		pause
+@echo off
+COLOR 7e
+cls
+REM :end
 goto begin
+REM :Error
+REM ECHO You did not enter correct number! Bye bye! Try again!
+
 
