944b41ab3ee0593575c27f04ec5bb5f36442fb7a
Jacob <1417473+J-ZOM@users.noreply.github.com>
2019-03-19 13:12:16 +0100

    v4

--- Call_number_by_adb_android.bat
+++ Call_number_by_adb_android.bat
@@ -1,5 +1,21 @@
 :author
-REM Author @jzomerfeld
+REM Author @Jakub_Zomerfeld : www.github.com/j-zom
+@echo off
+REM Deleting previously save file with device_id && reseting adb
+:reset_adb
+del device_id
+adb kill-server && adb start-server
+REM Saving a device_id=IMEI into a hashed file
+:define_device_id
+adb devices > device_id
+
+REM Print to console the defined device_id, %device_id% will be used later to pick a device which has to procced a call_command
+:print device_id
+@echo off
+for /f "tokens=1" %%a in (device_id) do set device_id=%%a
+echo Connected device:
+echo %device_id%
+
 :edit
 REM mode and following line define how big the console windows should be
 mode con:cols=65 lines=10
@@ -10,29 +26,43 @@ mode con:cols=65 lines=10
 
 :begin
 cls
-REM setlocal
+REM Function to use before pick a %number%
+echo/
+echo To reset ADB press - 1
+echo/
+REM Asking user for number to call / inserting the number variable [number] and call
+set /p number= Hey! What number would You like to call? || set number=NothingChosen
+REM SET _no_spaces=%_some_var: =%
 
-		REM Asking user for number to call / inserting the number variable [number] and -call?
-set /p number= Hey! What number would You like to call? || Set number=NothingChosen
-	If "%number%"=="NothingChosen" goto sub_error
-	If "%number%"==digits goto call
+REM Defining arguments
+If "%number%"=="1" goto reset_adb
+If "%number%"===%digits% goto call
+If "%number%"=="NothingChosen" goto sub_cls
+
+
+echo/
+echo Author @Jakub_Zomerfeld : www.github.com/j-zom
+echo/
 
 :call
 @echo off
-	adb -s "9885e6534e424f414e" shell am start -a android.intent.action.CALL -d tel:"%number%"
+REM	adb -s "9885e6534e424f414e" shell am start -a android.intent.action.CALL -d tel:"%number%"
+REM Previously defined device_id is used here in " "
+adb -s "%device_id%" shell am start -a android.intent.action.CALL -d tel:"%number%"
+
 :msg call
 color 1E
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛ 1%%
 echo ----------------------------------
 ping -n 1 localhost >nul
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ŰŰ˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛ 10%%
@@ -40,48 +70,48 @@ echo ----------------------------------
 ping -n 1 localhost >nul
 TIMEOUT /T 1 /NOBREAK > NUL
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ŰŰŰ˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛ 15%%
 echo ----------------------------------
 ping -n 1 localhost >nul
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ŰŰŰŰ˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛ 20%%
 echo ----------------------------------
 ping -n 1 localhost >nul
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ŰŰŰŰŰ˛˛˛˛˛˛˛˛˛˛˛˛˛˛˛ 25%%
 echo ----------------------------------
 ping -n 1 localhost >nul
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ŰŰŰŰŰŰ˛˛˛˛˛˛˛˛˛˛˛˛˛˛ 35%%
 echo ----------------------------------
 ping -n 1 localhost >nul
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ŰŰŰŰŰŰŰ˛˛˛˛˛˛˛˛˛˛˛˛˛ 45%%
 echo ----------------------------------
 ping -n 1 localhost >nul
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ŰŰŰŰŰŰŰŰŰŰ˛˛˛˛˛˛˛˛˛˛ 50%%
@@ -89,16 +119,16 @@ echo ----------------------------------
 ping -n 1 localhost >nul
 TIMEOUT /T 1 /NOBREAK > NUL
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ŰŰŰŰŰŰŰŰŰŰŰ˛˛˛˛˛˛˛˛˛ 55%%
 echo ----------------------------------
 ping -n 1 localhost >nul
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ŰŰŰŰŰŰŰŰŰŰŰŰ˛˛˛˛˛˛˛˛ 60%%
@@ -106,24 +136,24 @@ echo ----------------------------------
 ping -n 1 localhost >nul
 TIMEOUT /T 1 /NOBREAK > NUL
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ŰŰŰŰŰŰŰŰŰŰŰŰŰ˛˛˛˛˛˛˛ 65%%
 echo ----------------------------------
 ping -n 1 localhost >nul
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ŰŰŰŰŰŰŰŰŰŰŰŰŰŰ˛˛˛˛˛˛ 70%%
 echo ----------------------------------
 ping -n 1 localhost >nul
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰ˛˛˛˛ 80%%
@@ -131,39 +161,40 @@ echo ----------------------------------
 ping -n 1 localhost >nul
 TIMEOUT /T 1 /NOBREAK > NUL
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰ˛˛˛ 85%%
 echo ----------------------------------
 ping -n 1 localhost >nul
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰ˛˛ 90%%
 echo ----------------------------------
 ping -n 1 localhost >nul
 cls
-echo.
-echo.
+echo/
+echo/
 echo Calling... %number%
 echo ----------------------------------
 echo Progress: ŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰ˛ 95%%
 echo ----------------------------------
 ping -n 1 localhost >nul
 cls
-echo.
-echo.
+echo/
+echo/
 echo ----------------------------------
 echo Progress: ŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰŰ 100%%
 echo Call complete... %number%
-echo.
-echo.
-TIMEOUT /T 5		
-@echo off |clip
+echo/
+echo/
+TIMEOUT /T 3		
+REM @echo off |clip
+@echo off
 COLOR 7e
 cls
 
@@ -172,15 +203,11 @@ goto begin
 
 :sub_cls
 echo Nothing was chosen - cleaning
-TIMEOUT /T 5
+TIMEOUT /T 2
 goto begin
-
 
 :sub_error
-ECHO Invaid number! Bye!
+ECHO Invalid number! Bye!
 ECHO Try again!
-TIMEOUT /T 5
+TIMEOUT /T 2
 goto begin
-
-
-
